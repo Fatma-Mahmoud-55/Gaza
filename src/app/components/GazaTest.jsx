6b7aecc2dfadc778d5852ff6ef7faf85e4d6a950
Fatma-Mahmoud-55 <113183957+Fatma-Mahmoud-55@users.noreply.github.com>
2024-11-26 15:54:33 +0200

    Fatma test

--- src/app/components/GazaTest.jsx
+++ src/app/components/GazaTest.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+"use client";
 
-const GazaTest = () => {
+
+
+const GazaTest= () => {
+
+
+
     return (
-        <div>
-            dddddddddddddddddddddddddddddddddddd
-            dddddddddddddddddddddddddddddddddddd
-            dddddddddddddddddddddddddddddddddddd
-            dddddddddddddddddddddddddddddddddddd
-            dddddddddddddddddddddddddddddddddddd
-            dddddddddddddddddddddddddddddddddddd
-            dddddddddddddddddddddddddddddddddddd
-            dddddddddddddddddddddddddddddddddddd
-            dddddddddddddddddddddddddddddddddddd
-        </div>
+        <>
+            <h1 className="w-fit text-center font-semibold text-2xl p-3 bg-[#00997180] rounded-xl mx-auto my-10">
+                Countries serving the Gaza Strip
+            </h1>
+
+        </>
     );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 
 import GazaTest from "@/app/components/GazaTest";
+import Fatma from "@/app/components/Fatma";
 
 export default function Home() {
   return (<>
    Gaza
 
       <GazaTest/>
+      <Fatma/>
   </>);
 }
