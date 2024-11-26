54c53dc54e7185d131887f71894f647a9372ac89
Fatma-Mahmoud-55 <113183957+Fatma-Mahmoud-55@users.noreply.github.com>
2024-11-26 16:14:19 +0200

    last testt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 
 import GazaTest from "@/app/components/GazaTest";
-import Fatma from "@/app/components/Fatma";
 
 export default function Home() {
   return (<>
    Gaza
 
       <GazaTest/>
-      {/*<Fatma/>*/}
   </>);
 }
