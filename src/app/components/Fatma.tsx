3e9a56994bd0a5e0a30e7dea32f44b3391e67b6c
Fatma-Mahmoud-55 <113183957+Fatma-Mahmoud-55@users.noreply.github.com>
2024-11-26 16:03:26 +0200

    Fatma c

--- src/app/components/Fatma.tsx
+++ src/app/components/Fatma.tsx
@@ -141,140 +141,51 @@ const Fatma: React.FC = () => {
             <h1 className="w-fit text-center font-semibold text-2xl p-3 bg-[#00997180] rounded-xl mx-auto my-10">
                 Countries serving the Gaza Strip
             </h1>
-            <div className="flex">
-                {/* Map */}
-                <div className="w-3/4">
-                    <MapContainer
-                        center={[10, 10]}
-                        zoom={2}
-                        style={{ width: "100%", height: "500px" }}
-                    >
-                        <TileLayer
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        />
-                        {projectsData.results.map((item) => (
-                            <Marker
-                                key={item.country.id}
-                                position={[item.country.latitude, item.country.longitude]}
-                                icon={redIcon}
-                            >
-                                {selectedProjects[item.country.id]?.length > 0 && (
-                                    <Tooltip direction="top" permanent>
-                                        <div className="flex flex-wrap space-y-2">
-                                            <h3 className="font-bold">{item.country.name}</h3>
-                                            <div className="flex flex-row ">
-                                                {selectedProjects[item.country.id].map((project) => (
-                                                    <div
-                                                        key={project.id}
-                                                        className="bg-[#00994D25] p-4 rounded-md w-44 m-2"
-                                                    >
-                                                        <strong className="text-xl">{project.needs}</strong>
-                                                        <p>
-                                                            <strong>Number of Needs:</strong>{" "}
-                                                            {project.number_of_needs}
-                                                        </p>
-                                                        <p>
-                                                            <strong>Target Number:</strong>{" "}
-                                                            {project.target_number}
-                                                        </p>
-                                                        <p>
-                                                            <strong>Unit Cost:</strong> {project.unit_cost}
-                                                        </p>
-                                                        <p>
-                                                            <strong>Total Cost:</strong> {project.total_cost}
-                                                        </p>
-                                                        <p>
-                                                            <strong>Updated At:</strong> {project.updated_at}
-                                                        </p>
-                                                        <p className="whitespace-break-spaces">
-                                                            <strong>Details:</strong> {project.details}
-                                                        </p>
-                                                    </div>
-                                                ))}
-                                            </div>
-                                        </div>
-                                    </Tooltip>
-                                )}
-                            </Marker>
-                        ))}
-                    </MapContainer>
-                </div>
-
-                {/* Sidebar */}
-                <div
-                    style={{
-                        width: "25%",
-                        backgroundColor: "#d6f2e1",
-                        padding: "15px",
-                        borderLeft: "1px solid #ccc",
-                        overflowY: "auto",
-                    }}
+            <div className="w-3/4">
+                <MapContainer
+                    center={[10, 10]} // Still pass the center here
+                    zoom={2}            // Still pass the zoom level here
+                    style={{width: "100%", height: "500px"}}
+                    scrollWheelZoom={false} // Optionally disable zoom with the mouse wheel
                 >
-                    <h2 className="text-2xl font-semibold text-center my-3">Gaza Strip</h2>
-                    <input
-                        className="w-full rounded-md px-4 py-3 mb-5"
-                        placeholder="Search..."
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     />
                     {projectsData.results.map((item) => (
-                        <div key={item.country.id} dir="ltr" style={{ marginBottom: "20px" }}>
-                            <div
-                                style={{
-                                    display: "flex",
-                                    alignItems: "center",
-                                    justifyContent: "space-between",
-                                    cursor: "pointer",
-                                }}
-                                onClick={() => toggleCountry(item.country.id)}
-                            >
-                                <div
-                                    style={{
-                                        display: "flex",
-                                        alignItems: "center",
-                                        gap: "10px",
-                                    }}
-                                >
-                                    <h3 style={{ margin: 0 }}>{item.country.name}</h3>
-                                </div>
-                                <span
-                                    style={{
-                                        transform: expandedCountry.get(item.country.id)
-                                            ? "rotate(180deg)"
-                                            : "rotate(0deg)",
-                                        transition: "transform 0.2s",
-                                    }}
-                                >
-                  <MdOutlineKeyboardArrowDown />
-                </span>
-                            </div>
-                            {expandedCountry.get(item.country.id) && (
-                                <div style={{ marginLeft: "20px", marginTop: "10px" }}>
-                                    {item.projects.map((project) => (
-                                        <div
-                                            key={project.id}
-                                            style={{
-                                                display: "flex",
-                                                alignItems: "center",
-                                                marginBottom: "5px",
-                                                gap: "10px",
-                                            }}
-                                        >
-                                            <input
-                                                type="checkbox"
-                                                checked={selectedProjects[item.country.id]?.some(
-                                                    (p) => p.id === project.id
-                                                )}
-                                                onChange={() => handleProjectSelect(project, item.country)}
-                                            />
-                                            <span>{project.needs}</span>
+                        <Marker
+                            key={item.country.id}
+                            position={[item.country.latitude, item.country.longitude]}
+                            icon={redIcon}
+                        >
+                            {selectedProjects[item.country.id]?.length > 0 && (
+                                <Tooltip direction="top" permanent>
+                                    <div className="flex flex-wrap space-y-2">
+                                        <h3 className="font-bold">{item.country.name}</h3>
+                                        <div className="flex flex-row ">
+                                            {selectedProjects[item.country.id].map((project) => (
+                                                <div
+                                                    key={project.id}
+                                                    className="bg-[#00994D25] p-4 rounded-md w-44 m-2"
+                                                >
+                                                    <strong className="text-xl">{project.needs}</strong>
+                                                    <p>
+                                                        <strong>Number of Needs:</strong>{" "}
+                                                        {project.number_of_needs}
+                                                    </p>
+                                                    <p>
+                                                        <strong>Target Number:</strong>{" "}
+                                                        {project.target_number}
+                                                    </p>
+                                                </div>
+                                            ))}
                                         </div>
-                                    ))}
-                                </div>
+                                    </div>
+                                </Tooltip>
                             )}
-                            <hr style={{ border: "0.5px solid #ccc", margin: "15px 0" }} />
-                        </div>
+                        </Marker>
                     ))}
-                </div>
+                </MapContainer>
             </div>
         </>
     );
